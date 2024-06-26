49019f87bd93683449fffef87099bd6dc76ba2a0
begezidaann <rezazakie742@gmail.com>
2024-06-26 15:13:39 +0700

    NT bgt ges

--- resources/views/includes/navbar-admin.blade.php
+++ resources/views/includes/navbar-admin.blade.php
@@ -8,7 +8,7 @@
     <!-- * * * * * * When using an image, we recommend the SVG format.-->
     <!-- * * * * * * Dimensions: Maximum height: 32px, maximum width: 240px-->
     <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="{{ route('admin-dashboard') }}">
-        E-Arsip Surat
+        SIARS
     </a>
     <!-- Navbar Search Input-->
     <!-- * * Note: * * Visible only on and above the lg breakpoint-->
@@ -36,7 +36,7 @@
                     @if (Auth::user()->profile != NULL)
                         <img class="dropdown-user-img" src="{{ Storage::url(Auth::user()->profile) }}" />
                     @else
-                        <img class="dropdown-user-img" src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" />
+                        <img class="dropdown-user-img" src="{{ Auth::user()->name }}" />
                     @endif
 
                     <div class="dropdown-user-details">
@@ -45,7 +45,7 @@
                     </div>
                 </h6>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="{{ route('setting.index') }}">
                     <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
                     Account
                 </a>
